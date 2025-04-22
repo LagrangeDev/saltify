0532695eec818c4aad544c509c3e2cfe87065185
Seijo Cecilia <willamwei@outlook.com>
2025-04-22 11:05:31 +0800

    [core] fix QR code state query test

--- core/src/test/kotlin/org/ntqqrev/saltify/test/QrCodeLoginTest.kt
+++ core/src/test/kotlin/org/ntqqrev/saltify/test/QrCodeLoginTest.kt
@@ -1,31 +1,48 @@
 package org.ntqqrev.saltify.test
 
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import org.ntqqrev.saltify.BotContext
 import org.ntqqrev.saltify.common.Keystore
 import org.ntqqrev.saltify.operation.system.FetchQrCode
 import org.ntqqrev.saltify.operation.system.QueryQrCodeState
 import org.ntqqrev.saltify.packet.login.QrCodeState
 import org.ntqqrev.saltify.util.UrlSignProvider
-import java.lang.Thread.sleep
+import org.slf4j.LoggerFactory
+import kotlin.io.path.Path
+import kotlin.io.path.writeBytes
 
-suspend fun main() {
+val logger = LoggerFactory.getLogger("Test")
+
+suspend fun main() = coroutineScope {
     val signApiUrl = "https://sign.lagrangecore.org/api/sign/30366"
     val urlSignProvider = UrlSignProvider(signApiUrl)
     val appInfo = urlSignProvider.getAppInfo()
     val bot = BotContext(appInfo!!, Keystore.generateEmptyKeystore(), urlSignProvider)
     bot.ssoContext.connect()
 
+    val dataPath = Path("data")
+    if (!dataPath.toFile().exists()) {
+        dataPath.toFile().mkdirs()
+    }
+
     val fetchQrCodeResult = bot.callOperation(FetchQrCode)
-    println("QR code URL: ${fetchQrCodeResult.qrCodeUrl}")
+    logger.info("QR code URL: ${fetchQrCodeResult.qrCodeUrl}")
+    launch {
+        dataPath.resolve("qrcode.png").writeBytes(fetchQrCodeResult.qrCodePng)
+        logger.info("QR code saved to data/qrcode.png")
+    }
 
     while (true) {
         val qrCodeState = bot.callOperation(QueryQrCodeState)
+        logger.info("QrCodeState: ${qrCodeState.value}")
         if (qrCodeState == QrCodeState.WaitingForConfirm) {
-            println("User scanned the QR code")
-        } else if (qrCodeState == QrCodeState.Confirmed) {
+            logger.info("User scanned the QR code")
+        } else if (qrCodeState.value == QrCodeState.Confirmed.value) {
             break
         }
-        sleep(3000)
+        delay(3000)
     }
-    println("QR code has been confirmed")
+    logger.info("QR code has been confirmed")
 }
