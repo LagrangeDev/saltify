c0fd663dba3473dd5d6ef042f9de387ce9bd92a7
Wesley F. Young <willamwei@outlook.com>
2025-04-22 13:47:24 +0800

    [core] test logging in with existent session

---
+++ core/src/test/kotlin/org/ntqqrev/saltify/test/FastLoginTest.kt
@@ -0,0 +1,34 @@
+package org.ntqqrev.saltify.test
+
+import io.github.oshai.kotlinlogging.KotlinLogging
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.decodeFromStream
+import org.ntqqrev.saltify.BotContext
+import org.ntqqrev.saltify.common.Keystore
+import org.ntqqrev.saltify.operation.system.BotOnline
+
+private val logger = KotlinLogging.logger {  }
+
+suspend fun main() = coroutineScope {
+    val appInfo = urlSignProvider.getAppInfo()
+    val keystoreFile = keystorePath.toFile()
+    if (!keystoreFile.exists()) {
+        logger.error { "Keystore file not found, please run QrCodeLoginTest first" }
+        return@coroutineScope
+    }
+
+    val keystore = Json.decodeFromStream<Keystore>(keystoreFile.inputStream())
+    val bot = BotContext(appInfo!!, keystore, urlSignProvider)
+    bot.ssoContext.connect()
+
+    val onlineResult = bot.callOperation(BotOnline)
+    if (!onlineResult) {
+        logger.error { "Login failed" }
+        return@coroutineScope
+    }
+    logger.info { "Login successful" }
+
+    delay(Long.MAX_VALUE)
+}
