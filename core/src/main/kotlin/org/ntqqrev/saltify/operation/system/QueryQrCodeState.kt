6520ebf2a42cc6f2b50ed160c19e11e854808c49
Seijo Cecilia <willamwei@outlook.com>
2025-04-22 10:14:56 +0800

    [core] implement QueryQrCodeState

---
+++ core/src/main/kotlin/org/ntqqrev/saltify/operation/system/QueryQrCodeState.kt
@@ -0,0 +1,15 @@
+package org.ntqqrev.saltify.operation.system
+
+import org.ntqqrev.saltify.BotContext
+import org.ntqqrev.saltify.operation.NoInputOperation
+import org.ntqqrev.saltify.packet.login.QrCodeState
+
+object QueryQrCodeState : NoInputOperation<QrCodeState> {
+    override val command = "wtlogin.trans_emp"
+
+    override fun build(bot: BotContext, payload: Unit): ByteArray
+        = bot.wtLoginContext.buildTransEmp0x12()
+
+    override fun parse(bot: BotContext, payload: ByteArray): QrCodeState
+        = bot.wtLoginContext.parseTransEmp0x12(payload)
+}
--- core/src/test/kotlin/org/ntqqrev/saltify/test/QrCodeLoginTest.kt
+++ core/src/test/kotlin/org/ntqqrev/saltify/test/QrCodeLoginTest.kt
@@ -3,7 +3,10 @@ package org.ntqqrev.saltify.test
 import org.ntqqrev.saltify.BotContext
 import org.ntqqrev.saltify.common.Keystore
 import org.ntqqrev.saltify.operation.system.FetchQrCode
+import org.ntqqrev.saltify.operation.system.QueryQrCodeState
+import org.ntqqrev.saltify.packet.login.QrCodeState
 import org.ntqqrev.saltify.util.UrlSignProvider
+import java.lang.Thread.sleep
 
 suspend fun main() {
     val signApiUrl = "https://sign.lagrangecore.org/api/sign/30366"
@@ -11,6 +14,18 @@ suspend fun main() {
     val appInfo = urlSignProvider.getAppInfo()
     val bot = BotContext(appInfo!!, Keystore.generateEmptyKeystore(), urlSignProvider)
     bot.ssoContext.connect()
+
     val fetchQrCodeResult = bot.callOperation(FetchQrCode)
     println("QR code URL: ${fetchQrCodeResult.qrCodeUrl}")
+
+    while (true) {
+        val qrCodeState = bot.callOperation(QueryQrCodeState)
+        if (qrCodeState == QrCodeState.WaitingForConfirm) {
+            println("User scanned the QR code")
+        } else if (qrCodeState == QrCodeState.Confirmed) {
+            break
+        }
+        sleep(3000)
+    }
+    println("QR code has been confirmed")
 }
