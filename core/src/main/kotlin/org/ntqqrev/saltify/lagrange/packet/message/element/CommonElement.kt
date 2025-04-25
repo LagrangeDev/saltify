3c8661b504fac709e0323c3fbefae0d52f4280c3
Seijo Cecilia <willamwei@outlook.com>
2025-04-25 21:58:25 +0800

    [core] CommonElement.pbElem is never null

--- core/src/main/kotlin/org/ntqqrev/saltify/lagrange/packet/message/element/CommonElement.kt
+++ core/src/main/kotlin/org/ntqqrev/saltify/lagrange/packet/message/element/CommonElement.kt
@@ -6,6 +6,6 @@ import kotlinx.serialization.protobuf.ProtoNumber
 @Serializable
 class CommonElement(
     @ProtoNumber(1) val serviceType: Int,
-    @ProtoNumber(2) val pbElem: ByteArray?,
+    @ProtoNumber(2) val pbElem: ByteArray,
     @ProtoNumber(3) val businessType: Long,
 ) : ElementType
