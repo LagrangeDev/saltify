beaa7ba16ff03878e26ef53eea7b89d25d6f65e3
Seijo Cecilia <willamwei@outlook.com>
2025-04-25 22:37:22 +0800

    [adapter] config should be variable

--- adapter/src/main/kotlin/org/ntqqrev/saltify/lagrange/adapter/LagrangeInit.kt
+++ adapter/src/main/kotlin/org/ntqqrev/saltify/lagrange/adapter/LagrangeInit.kt
@@ -6,5 +6,5 @@ import org.ntqqrev.saltify.api.Config
 @Serializable
 class LagrangeInit {
     @Config("签名 API URL")
-    val signApiUrl: String = "https://sign.lagrangecore.org/api/sign/30366"
+    var signApiUrl: String = "https://sign.lagrangecore.org/api/sign/30366"
 }
