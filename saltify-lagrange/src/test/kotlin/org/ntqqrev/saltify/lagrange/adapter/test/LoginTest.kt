b8bbcc7a8fad82e3d3fb9cf94ed0ff1b0cf0640d
Seijo Cecilia <willamwei@outlook.com>
2025-04-30 13:31:54 +0800

    [adapter] remove redundant test code

--- saltify-lagrange/src/test/kotlin/org/ntqqrev/saltify/lagrange/adapter/test/LoginTest.kt
+++ saltify-lagrange/src/test/kotlin/org/ntqqrev/saltify/lagrange/adapter/test/LoginTest.kt
@@ -1,20 +1,13 @@
 package org.ntqqrev.saltify.lagrange.adapter.test
 
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.filterIsInstance
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
 import org.ntqqrev.saltify.lagrange.adapter.LagrangeContextFactory
 import org.ntqqrev.saltify.lagrange.adapter.LagrangeInit
-import org.ntqqrev.saltify.lagrange.event.MessagePushEvent
 
-suspend fun main(): Unit {
-    val scope = CoroutineScope(testEnv.parentCoroutineContext)
-    scope.run {
+suspend fun main() {
+    CoroutineScope(testEnv.parentCoroutineContext).run {
         val ctx = LagrangeContextFactory.createContext(
             init = LagrangeInit(),
             env = testEnv,
@@ -24,13 +17,6 @@ suspend fun main(): Unit {
 
         ctx.getAllFriends().forEach { println(it) }
         ctx.getAllGroups().forEach { println(it) }
-
-        ctx.lagrange.eventFlow
-            .filterIsInstance<MessagePushEvent>()
-            .collect {
-                println(it.push.responseHead.groupExt?.groupName)
-                println(it.push.contentHead.sequence)
-            }
 
         delay(Long.MAX_VALUE)
     }
