88a3eaa080381de93a716dbdeb496b57665e9da8
Seijo Cecilia <willamwei@outlook.com>
2025-05-01 14:40:11 +0800

    [adapter] reformat code

--- saltify-lagrange/src/main/kotlin/org/ntqqrev/saltify/lagrange/adapter/message/incoming/LagrangeSegmentFactory.kt
+++ saltify-lagrange/src/main/kotlin/org/ntqqrev/saltify/lagrange/adapter/message/incoming/LagrangeSegmentFactory.kt
@@ -1,7 +1,6 @@
 package org.ntqqrev.saltify.lagrange.adapter.message.incoming
 
 import org.ntqqrev.saltify.api.context.message.incoming.segment.Segment
-import org.ntqqrev.saltify.lagrange.packet.message.MessageElement
 
 interface LagrangeSegmentFactory<T : Segment> {
     fun tryParse(reader: ElementReader): T?
