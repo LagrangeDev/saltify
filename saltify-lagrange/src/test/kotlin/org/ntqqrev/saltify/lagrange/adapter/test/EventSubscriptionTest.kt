8355ed561097eef25011eefd65f43ff4c61b2bad
Seijo Cecilia <willamwei@outlook.com>
2025-05-01 14:39:37 +0800

    [adapter] add tests for message

--- saltify-lagrange/src/test/kotlin/org/ntqqrev/saltify/lagrange/adapter/test/EventSubscriptionTest.kt
+++ saltify-lagrange/src/test/kotlin/org/ntqqrev/saltify/lagrange/adapter/test/EventSubscriptionTest.kt
@@ -1,5 +1,6 @@
 package org.ntqqrev.saltify.lagrange.adapter.test
 
+import io.github.oshai.kotlinlogging.KotlinLogging
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.filterIsInstance
@@ -7,6 +8,8 @@ import kotlinx.coroutines.launch
 import org.ntqqrev.saltify.api.context.event.message.MessageReceiveEvent
 import org.ntqqrev.saltify.lagrange.adapter.LagrangeContextFactory
 import org.ntqqrev.saltify.lagrange.adapter.LagrangeInit
+
+private val logger = KotlinLogging.logger {  }
 
 suspend fun main() {
     testEnv.scope.run {
@@ -20,7 +23,7 @@ suspend fun main() {
         launch {
             ctx.channel.filterIsInstance<MessageReceiveEvent>()
                 .collect {
-                    println("Received message ${it.messageId}")
+                    logger.info { it.message }
                 }
         }
 
