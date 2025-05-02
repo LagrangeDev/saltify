ca8cbcaa9b8d1d911d36ecf8762d52b7e4f05fff
Seijo Cecilia <willamwei@outlook.com>
2025-05-03 01:00:42 +0800

    [core] hardcode default values into IndexNode

--- saltify-lagrange-core/src/main/kotlin/org/ntqqrev/saltify/lagrange/packet/highway/IndexNode.kt
+++ saltify-lagrange-core/src/main/kotlin/org/ntqqrev/saltify/lagrange/packet/highway/IndexNode.kt
@@ -4,10 +4,10 @@ import org.ntqqrev.saltify.protobuf.ProtoMessage
 import org.ntqqrev.saltify.protobuf.annotation.ProtoField
 
 class IndexNode(
-    @ProtoField(1) var info: FileInfo?,
-    @ProtoField(2) var fileUuid: String?,
-    @ProtoField(3) var storeId: Long,
-    @ProtoField(4) var uploadTime: Long,
+    @ProtoField(1) var info: FileInfo? = null,
+    @ProtoField(2) var fileUuid: String,
+    @ProtoField(3) var storeId: Int = 1,
+    @ProtoField(4) var uploadTime: Long = 0,
     @ProtoField(5) var ttl: Long,
-    @ProtoField(6) var subType: Long,
+    @ProtoField(6) var subType: Long = 0,
 ) : ProtoMessage()
