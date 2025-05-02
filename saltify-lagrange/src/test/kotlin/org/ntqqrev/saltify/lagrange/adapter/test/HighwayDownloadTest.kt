33e88c22d4fe19a55d15c3917cdc54c13c825f4f
Seijo Cecilia <willamwei@outlook.com>
2025-05-03 01:01:33 +0800

    [adapter] add tests on get private image url

---
+++ saltify-lagrange/src/test/kotlin/org/ntqqrev/saltify/lagrange/adapter/test/HighwayDownloadTest.kt
@@ -0,0 +1,40 @@
+package org.ntqqrev.saltify.lagrange.adapter.test
+
+import io.github.oshai.kotlinlogging.KotlinLogging
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.filterIsInstance
+import kotlinx.coroutines.launch
+import org.ntqqrev.saltify.api.context.event.message.MessageReceiveEvent
+import org.ntqqrev.saltify.api.context.message.incoming.PrivateIncomingMessage
+import org.ntqqrev.saltify.api.context.message.incoming.segment.ImageSegment
+import org.ntqqrev.saltify.lagrange.adapter.LagrangeContextFactory
+import org.ntqqrev.saltify.lagrange.adapter.LagrangeInit
+
+private val logger = KotlinLogging.logger {  }
+
+suspend fun main() {
+    testEnv.scope.run {
+        val ctx = LagrangeContextFactory.createContext(
+            init = LagrangeInit(),
+            env = testEnv,
+            flow = MutableSharedFlow()
+        )
+        ctx.start()
+
+        launch {
+            ctx.channel.filterIsInstance<MessageReceiveEvent>()
+                .collect {
+                    if (it.message is PrivateIncomingMessage) {
+                        logger.info { "Private -> " + it.message }
+                        it.message.segments.filterIsInstance<ImageSegment>()
+                            .forEach { segment ->
+                                println(ctx.getResourceTempUrl(segment.resourceId))
+                            }
+                    }
+                }
+        }
+
+        delay(Long.MAX_VALUE)
+    }
+}
