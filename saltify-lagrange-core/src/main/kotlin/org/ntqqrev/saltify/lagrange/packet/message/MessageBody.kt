6e9f9f080fd038088d4b748cacd08309295c1756
Seijo Cecilia <willamwei@outlook.com>
2025-05-13 11:05:05 +0800

    [core] rollback making `richText` nullable

--- saltify-lagrange-core/src/main/kotlin/org/ntqqrev/saltify/lagrange/packet/message/MessageBody.kt
+++ saltify-lagrange-core/src/main/kotlin/org/ntqqrev/saltify/lagrange/packet/message/MessageBody.kt
@@ -4,7 +4,7 @@ import org.ntqqrev.saltify.protobuf.ProtoMessage
 import org.ntqqrev.saltify.protobuf.annotation.ProtoField
 
 class MessageBody(
-    @ProtoField(1) var richText: RichText? = null,
+    @ProtoField(1) var richText: RichText,
     @ProtoField(2) var msgContent: ByteArray? = null,
     @ProtoField(3) var msgEncryptContent: ByteArray? = null,
 ) : ProtoMessage()
